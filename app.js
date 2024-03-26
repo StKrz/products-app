68e60a9737326aba5ce4f3c81fb402250b118c93
StKrz <stavroskyriazopoulos@gmail.com>
2024-03-26 06:02:49 +0200

    add product

--- app.js
+++ app.js
@@ -24,10 +24,15 @@ require('dotenv').config()
 
    const user = require('./routes/user.route');
    const userProduct = require('./routes/user.products.routes')
-   
+   const product = require('./routes/product.route')
+
+
    app.use('/', express.static('files'))
    app.use('/api/users', user)
    app.use('/api/user-products', userProduct)
+
+   app.use('/api/products', product)
+
    
    app.use('/api-docs', 
      swaggerUi.serve,
